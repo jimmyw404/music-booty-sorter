da97c84dade941e4fb788cc6c99175dbf87aa682
James Walker <jwalker@quantumsignalai.com>
2023-04-12 11:38:55 -0400

    misc

--- cleaner.py
+++ cleaner.py
@@ -11,6 +11,21 @@ class Cleaner:
         # Remove  "01. " from "01. Introduction.mp3"
         self.regexes.append(r'^\d{2}\. ')
 
+        # Remove "01-" from "01-Death Tone.mp3"
+        self.regexes.append(r'^\d{2}\-')
+
+        # Remove "01 - " from "01 - The Lord Of Steel.Mp3"
+        self.regexes.append(r'^\d{2} - ')
+
+        # Remove "03 – " from "03 – Never Forgotten Heroes.Mp3"
+        self.regexes.append(r'^\d{2} – ')
+
+        # Remove "02.- " from "02.- Emerald Sword.Mp3"
+        self.regexes.append(r'^\d{2}\.-\s+')
+
+        # Remove "03 " from "03 Queen Of The Dark Horizons.Mp3"
+        self.regexes.append(r'^\d{2}\s')
+
     def is_song(self, filename):
         if filename.endswith(".mp3"):
             return True
--- mbs.py
+++ mbs.py
@@ -1,8 +1,8 @@
 import os
 import combiner
 
-src_directory = "H:/m/Sabaton/food"
-dst_directory = "H:/m/Sabaton/fav"
+src_directory = "H:/m/Rhapsody of Fire/fav_food"
+dst_directory = "H:/m/Rhapsody of Fire/fav"
 
 comb = combiner.Combiner(src_directory, dst_directory)
 comb.combine()
